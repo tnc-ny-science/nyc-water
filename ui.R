5b7217f8ebf442e003198f0b8135311836af40cc
Mike Treglia <mtreglia@gmail.com>
2018-11-29 20:38:26 -0500

    Made code functional for just running a test visualization

--- ui.R
+++ ui.R
@@ -147,15 +147,15 @@ dashboardPage(header, sidebar, body)
 
 
 
-# server <- function(input, output) {
-#   set.seed(122)
-#   histdata <- rnorm(500)
-#   
-#   output$plot1 <- renderPlot({
-#     data <- histdata[seq_len(input$slider)]
-#     hist(data)
-#   })
-# }
+server <- function(input, output) {
+  set.seed(122)
+  histdata <- rnorm(500)
+
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
 
 shinyApp(dashboardPage(header, sidebar, body), server)
 
